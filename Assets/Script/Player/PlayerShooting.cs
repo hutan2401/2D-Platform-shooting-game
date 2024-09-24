3986266ad7a139a7b14f932b0b34e26da97d776e
KhoaNguyenViet <vkhoa2296@gmail.com>
2024-09-24 18:27:21 +0700

    fix

--- Assets/Script/Player/PlayerShooting.cs
+++ Assets/Script/Player/PlayerShooting.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pistol : MonoBehaviour
+public class PlayerShooting : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
