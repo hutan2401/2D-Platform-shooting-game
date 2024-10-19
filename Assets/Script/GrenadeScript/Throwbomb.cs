bad2f07b050bb87fdb2b171a0ea1134164974d02
KhoaNguyenViet <vkhoa2296@gmail.com>
2024-10-19 13:04:49 +0700

    update grenade Ammo box

--- Assets/Script/GrenadeScript/Throwbomb.cs
+++ Assets/Script/GrenadeScript/Throwbomb.cs
@@ -7,16 +7,21 @@ public class Throwbomb : MonoBehaviour
     [SerializeField] private float throwForce = 6f;
     [SerializeField] private Transform transFormPointGrenade;
     [SerializeField] private GameObject grenadePrefab;
+    [SerializeField] private int maxGrenade = 10;
+    private int currentGrenade;
+
 
     private PlayerController playerController;
 
     private void Awake()
     {
         playerController = new PlayerController();
+
     }
 
     private void Start()
     {
+        currentGrenade = maxGrenade;
         playerController.Player.ThrowGrenade.performed += _ => ThrowGrenade();
     }
 
@@ -33,9 +38,20 @@ public class Throwbomb : MonoBehaviour
 
     public void ThrowGrenade()
     {
-        GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
-        Rigidbody2D rb =  grenade.GetComponent<Rigidbody2D>();
-        var direction = transform.right + Vector3.up;
-        rb.AddForce(direction * throwForce, ForceMode2D.Impulse);
+       if(currentGrenade >0)
+        {
+            GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+            Rigidbody2D rb = grenade.GetComponent<Rigidbody2D>();
+            var direction = transform.right + Vector3.up;
+            rb.AddForce(direction * throwForce, ForceMode2D.Impulse);
+            currentGrenade--;
+            Debug.Log("grenade: "+ currentGrenade);
+        }
+    }
+
+    public void AddAGrenade()
+    {
+        currentGrenade += 10;
+        Debug.Log("Add" +currentGrenade);
     }
 }
