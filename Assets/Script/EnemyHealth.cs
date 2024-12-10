5d23fc5e2bf233e4a3a74dac854674cd38591bb2
KhoaNguyenViet <vkhoa2296@gmail.com>
2024-12-10 20:43:05 +0700

    fix

---
+++ Assets/Script/EnemyHealth.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int enemyHealth = 1;
+
+    private int currentHealth;
+
+    public UnityEvent OnEnemyDeath;
+    public int CurrentHealth => currentHealth;
+    public int MaxHealth => enemyHealth;
+    private void Start()
+    {
+        currentHealth = enemyHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        OnEnemyDeath?.Invoke();
+        Destroy(gameObject, 1f);
+    }
+}
